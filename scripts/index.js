ed585a33c2c875e7bd613053cb332c3d4cf92dd0
Robin Khiv <robinkhiv@gmail.com>
2019-01-24 08:20:17 -0800

    modified files

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,4 @@ $(document).ready(function() {
 });
 console.log(store);
 console.log(foo);
+console.log(Item);
--- scripts/store.js
+++ scripts/store.js
@@ -1,11 +1,17 @@
 const store = (function () {
-    const foo = bar; 
-    items: [
+    // const foo = 'bar'; 
+    const items = [
         { id: cuid(), name: 'apples', checked: false },
         { id: cuid(), name: 'oranges', checked: false },
         { id: cuid(), name: 'milk', checked: true },
         { id: cuid(), name: 'bread', checked: false }
-      ],
-      hideCheckedItems: false,
-      searchTerm: ''
-}() );
+      ];
+    const hideCheckedItems = false;
+    const searchTerm = '';
+
+    return {
+     items,
+     hideCheckedItems,
+     searchTerm
+    };
+}());
